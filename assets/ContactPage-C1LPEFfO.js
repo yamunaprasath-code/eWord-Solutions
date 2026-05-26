6949219e49d1508ddda1f45cc4956fe78cbf690b
WFH <WFH@DESKTOP-0S0UMDR.RAPIDCARE.COM>
2026-05-26 17:07:09 +0530

    Updates

---
+++ assets/ContactPage-C1LPEFfO.js
@@ -0,0 +1,1 @@
+import{a as e}from"./vendor-motion-BubtLuRV.js";import{t}from"./MainLayout-B4l-Ma-T.js";import{t as n}from"./contact-df28fmlV.js";var r=e();function i(){return(0,r.jsx)(t,{children:(0,r.jsx)(`div`,{className:`pt-[72px]`,children:(0,r.jsx)(n,{})})})}export{i as default};
