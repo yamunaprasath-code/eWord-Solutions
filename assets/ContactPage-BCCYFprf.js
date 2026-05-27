1572951277581d10a5c5fe34c32a1ea76012fed1
WFH <WFH@DESKTOP-0S0UMDR.RAPIDCARE.COM>
2026-05-27 15:28:45 +0530

    Updates

---
+++ assets/ContactPage-BCCYFprf.js
@@ -0,0 +1,1 @@
+import{a as e}from"./vendor-motion-BubtLuRV.js";import{t}from"./MainLayout-CYK1bIHY.js";import{t as n}from"./contact-D0ArjeS5.js";var r=e();function i(){return(0,r.jsx)(t,{children:(0,r.jsx)(`div`,{className:`pt-[72px]`,children:(0,r.jsx)(n,{})})})}export{i as default};
